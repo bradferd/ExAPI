d450d479570bb54a1ca266baf8ce77c6ae3b0256
bradferd <bbailey921@gmail.com>
2020-05-06 10:46:58 -0400

    Add models for Menu, Submenu and Reservation classes

---
+++ ExpressoAPI/Models/Submenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressoAPI.Models
+{
+    public class Submenu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Piece { get; set; }
+        public string Image { get; set; }
+
+    }
+}
