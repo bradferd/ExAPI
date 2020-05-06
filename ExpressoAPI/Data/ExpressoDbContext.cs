84447dd87c6c28b9edb0aea047431e6a89ea7055
bradferd <bbailey921@gmail.com>
2020-05-06 10:58:20 -0400

    Add data annotations for Reservation model and db connection string

---
+++ ExpressoAPI/Data/ExpressoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExpressoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressoAPI.Data
+{
+    public class ExpressoDbContext:DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+    }
+}
--- ExpressoAPI/Models/Reservation.cs
+++ ExpressoAPI/Models/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,26 @@ namespace ExpressoAPI.Models
     public class Reservation
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]*$")]
         public string Phone { get; set; }
+
+        [Required]
         public int TotalPeople { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public string Time { get; set; }
 
     }
